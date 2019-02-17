5961f1393f2544a41bc35cd0194399f59864afcf
mflavin <mflavin@hawk.iit.edu>
2019-02-16 23:45:21 -0600

    Changed the homepage to salary index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'salaries#index', as: 'salaries_index'
+  resources :salaries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
