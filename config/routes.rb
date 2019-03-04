58eff43f1ade680af496b5fe9d679399144bdcfd
mflavin <mflavin@hawk.iit.edu>
2019-03-03 18:25:50 -0600

    Changed root index location off from old location

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'salaries#index', as: 'salaries_index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
