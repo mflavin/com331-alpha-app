305a93e5c4dcd45d1b1124a4dd273e97ebcaf860
mflavin <mflavin@hawk.iit.edu>
2019-03-14 20:41:59 -0500

    Adding routes access to allow for nesting.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
-  resources :days
-  resources :weeks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'weeks#index'
+
+  resources :weeks do
+    resources :days
+  end
+
 end
