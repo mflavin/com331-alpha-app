21c439e4de04329bf60fb546795e699360c0a9c1
mflavin <mflavin@hawk.iit.edu>
2019-03-14 20:44:05 -0500

    Referenced week and then used week_id to reference day. Lots of change, but basically the same but nesting the data.

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -4,32 +4,54 @@ class DaysController < ApplicationController
   # GET /days
   # GET /days.json
   def index
-    @days = Day.all
+    week = Week.find(params[:week_id])
+    @days = week.days
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @days }
+    end
   end
 
   # GET /days/1
   # GET /days/1.json
   def show
+    week = Week.find(params[:week_id])
+    @day = week.days.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @days }
+    end
   end
 
   # GET /days/new
   def new
-    @day = Day.new
+    week = Week.find(params[:week_id])
+    @day = week.days.build
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @days }
+    end
   end
 
   # GET /days/1/edit
   def edit
+    week = Week.find(params[:week_id])
+    @day = week.days.find(params[:id])
   end
 
   # POST /days
   # POST /days.json
   def create
-    @day = Day.new(day_params)
+    week = Week.find(params[:week_id])
+    @day = week.days.new(day_params)
 
     respond_to do |format|
       if @day.save
-        format.html { redirect_to @day, notice: 'Day was successfully created.' }
-        format.json { render :show, status: :created, location: @day }
+        format.html { redirect_to ([@day.week, @day]), notice: 'Day was successfully created.' }
+        format.json { render :show, status: :created, location: [@day.week, @day] }
       else
         format.html { render :new }
         format.json { render json: @day.errors, status: :unprocessable_entity }
@@ -40,10 +62,13 @@ class DaysController < ApplicationController
   # PATCH/PUT /days/1
   # PATCH/PUT /days/1.json
   def update
+    week = Week.find(params[:week_id])
+    @day = week.days.find(params[:id])
+
     respond_to do |format|
       if @day.update(day_params)
-        format.html { redirect_to @day, notice: 'Day was successfully updated.' }
-        format.json { render :show, status: :ok, location: @day }
+        format.html { redirect_to ([@day.week, @day]), notice: 'Day was successfully updated.' }
+        format.json { render :show, status: :ok, location: ([@day.week, @day]) }
       else
         format.html { render :edit }
         format.json { render json: @day.errors, status: :unprocessable_entity }
@@ -54,9 +79,12 @@ class DaysController < ApplicationController
   # DELETE /days/1
   # DELETE /days/1.json
   def destroy
+    week = Week.find(params[:week_id])
+    @day = week.days.find(params[:id])
+
     @day.destroy
     respond_to do |format|
-      format.html { redirect_to days_url, notice: 'Day was successfully destroyed.' }
+      format.html { redirect_to week_days_url, notice: 'Day was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
