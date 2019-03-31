18e7b01500f08ebd179ddb989d8f1b2b1d3ad4f2
mflaivn <mflavin@hawk.iit.edu>
2019-03-31 17:20:10 -0500

    Allowed for deleting with foregin keys

--- db/migrate/20190331142550_create_weight_sets.rb
+++ db/migrate/20190331142550_create_weight_sets.rb
@@ -2,7 +2,7 @@ class CreateWeightSets < ActiveRecord::Migration[5.2]
   def change
     create_table :weight_sets do |t|
       t.integer :numb_of_sets
-      t.references :workout, index: true, foreign_key: true
+      t.references :workout, index: true, foreign_key: {on_delete: :cascade}
       t.timestamps
     end
   end
