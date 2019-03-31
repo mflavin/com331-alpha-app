d01c9bc09c1bb151cf3708167781e888ea98b9d3
mflaivn <mflavin@hawk.iit.edu>
2019-03-31 11:36:22 -0500

    Added validation to models

--- app/models/weight_set.rb
+++ app/models/weight_set.rb
@@ -1,4 +1,7 @@
 class WeightSet < ApplicationRecord
+  validates :numb_of_sets, presence: true
+  validates :numb_of_sets, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+
   belongs_to :workout
   has_many :weight_reps
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,5 @@
 class Workout < ApplicationRecord
+  validates :workout_name, presence: true
+
   has_many :weight_sets
 end
